475eecd2d40f3953db61fdceb69c42780f71b142
Victor Yang <vyang11@my.bcit.ca>
2025-02-15 13:50:05 -0800

    removed test messages from server.js

--- js/server.js
+++ js/server.js
@@ -5,13 +5,6 @@ const mysql = require('mysql2');
 const url = require('url');
 const message = require('../lang/messages/en/user');
 require('dotenv').config();
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_PORT:", process.env.DB_PORT);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("DB_NAME:", process.env.DB_NAME);
-console.log("CA_CERT_PATH:", process.env.CA_CERT_PATH);
-
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
