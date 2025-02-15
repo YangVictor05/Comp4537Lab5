fb088682ce9710072df45d3331e28bd91cae55ec
Victor Yang <vyang11@my.bcit.ca>
2025-02-15 13:37:35 -0800

    Removed console.logs for digitalocean testing

--- js/server.js
+++ js/server.js
@@ -6,12 +6,6 @@ const url = require('url');
 const message = require('../lang/messages/en/user');
 require('dotenv').config();
 
-console.log("DB_HOST: ", process.env.DB_HOST);
-console.log("DB_USER: ", process.env.DB_USER);
-console.log("DB_PASSWORD: ", process.env.DB_PASSWORD);
-console.log("DB_NAME: ", process.env.DB_NAME);
-console.log("CA_CERT_PATH: ", process.env.CA_CERT_PATH);
-
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
