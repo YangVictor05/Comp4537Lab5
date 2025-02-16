5ef008c3f3d177eb3229673a0cd8693505684b48
Victor Yang <vyang11@my.bcit.ca>
2025-02-16 13:09:52 -0800

    Returned CORS headers

--- js/server.js
+++ js/server.js
@@ -37,6 +37,8 @@ function createTable(callback) {
 const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.setHeader("Content-Type", "application/json");
 
     if (req.method === 'OPTIONS') return res.writeHead(200).end();
